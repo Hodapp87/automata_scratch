dfbfbe19f2a46a86402de35c23a93a370d8dee21
hodapp <hodapp87@gmail.com>
2019-09-17 12:29:41 -0400

    Add transform stack with matrices

--- meshutil.py
+++ meshutil.py
@@ -18,7 +18,7 @@ rtb = numpy.array([1,1,1])
 
 class FaceVertexMesh(object):
     def __init__(self, v, f):
-        # v & f should both be of shape (N,3)ll
+        # v & f should both be of shape (N,3)
         self.v = v
         self.f = f
     def concat(self, other_mesh):
@@ -27,6 +27,12 @@ class FaceVertexMesh(object):
         f2 = numpy.concatenate([self.f, other_mesh.f + self.v.shape[0]])
         m2 = FaceVertexMesh(v2, f2)
         return m2
+    def transform(self, mtx):
+        vh = numpy.hstack([self.v, numpy.ones((self.v.shape[0], 1), dtype=self.v.dtype)])
+        v2 = vh.dot(mtx.T)[:,0:3]
+        # TODO: why transpose?
+        # TODO: fix homogenous (even if I don't use it)
+        return FaceVertexMesh(v2, self.f)
     def to_stl_mesh(self):
         data = numpy.zeros(self.f.shape[0], dtype=stl.mesh.Mesh.dtype)
         v = data["vectors"]
@@ -34,11 +40,51 @@ class FaceVertexMesh(object):
             v[i] = [self.v[iv0], self.v[iv1], self.v[iv2]]
         return stl.mesh.Mesh(data)
 
+class TransformStack(object):
+    def __init__(self, mtx=None):
+        if mtx is None:
+            self.mtx = numpy.identity(4)
+        else:
+            self.mtx = mtx
+    def scale(self, *a, **kw):
+        mtx = mtx_scale(*a, **kw)
+        return TransformStack(mtx @ self.mtx)
+    def translate(self, *a, **kw):
+        mtx = mtx_translate(*a, **kw)
+        return TransformStack(mtx @ self.mtx)
+    def rotate(self, *a, **kw):
+        mtx = mtx_rotate(*a, **kw)
+        return TransformStack(mtx @ self.mtx)
+
+def mtx_scale(sx, sy=None, sz=None):
+    if sy is None:
+        sy = sx
+    if sz is None:
+        sz = sx
+    return numpy.array([
+        [sx, 0,  0,  0],
+        [0, sy,  0,  0],
+        [0,  0, sz,  0],
+        [0,  0,  0,  1],
+    ])
+
+def mtx_translate(x, y, z):
+    return numpy.array([
+        [1, 0, 0, x],
+        [0, 1, 0, y],
+        [0, 0, 1, z],
+        [0, 0, 0, 1],
+    ])
+
+def mtx_rotate(axis, angle):
+    q = quat.rotation_quaternion(axis, angle)
+    return quat.quat2mat(q)
+
 def cube(open_xz=False):
     verts = numpy.array([
         lbf, rbf, ltf, rtf,
         lbb, rbb, ltb, rtb,
-    ], dtype=numpy.float32)
+    ], dtype=numpy.float64)
     if open_xz:
         faces = numpy.zeros((8,3), dtype=int)
     else:
@@ -69,7 +115,7 @@ def cube_distort(angle):
     verts = numpy.array([
         lbf, rbf, ltf2, rtf2,
         lbb, rbb, ltb2, rtb2,
-    ], dtype=numpy.float32)
+    ], dtype=numpy.float64)
     if True: #open_xz:
         faces = numpy.zeros((8,3), dtype=int)
     else:
--- quat.py
+++ quat.py
@@ -15,10 +15,19 @@ def rotation_quaternion(axis, angle):
     """
     qc = numpy.cos(angle / 2)
     qs = numpy.sin(angle / 2)
-    qv = qs * axis
+    qv = qs * numpy.array(axis)
     return numpy.quaternion(qc, qv[0], qv[1], qv[2])
 
 def vec2quat(vs):
     qs = numpy.zeros(vs.shape[0], dtype=numpy.quaternion)
     quaternion.as_float_array(qs)[:,1:4] = vs
     return qs
+
+def quat2mat(q):
+    s = 1
+    return numpy.array([
+        [1-2*s*(q.y**2+q.z**2), 2*s*(q.x*q.y-q.z*q.w), 2*s*(q.x*q.z+q.y*q.w), 0],
+        [2*s*(q.x*q.y+q.z*q.w), 1-2*s*(q.x**2+q.z**2), 2*s*(q.y*q.z-q.x*q.w), 0],
+        [2*s*(q.x*q.z-q.y*q.w), 2*s*(q.y*q.z+q.x*q.w), 1-2*s*(q.x**2+q.y**2), 0],
+        [0, 0, 0, 1],
+    ])
